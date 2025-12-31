9ac2657606f0bc46c78c79f75d6ca8b8c46fddee
shusan65 <bcasusan123@gmail.com>
2025-12-31 12:14:52 +0545

    second commit

--- mypp/urls.py
+++ mypp/urls.py
@@ -3,7 +3,7 @@ from.views.mainview import index_page
 from.views.authview import register_method,login_method
 
 urlpatterns=[
-     path('index/',index_page,name="index"),
+     path('',index_page,name="index"),
      path('login/',login_method,name='login'),
-     path('',register_method,name='register')
+     path('register/',register_method,name='register')
     ]
--- mypp/views/authview.py
+++ mypp/views/authview.py
@@ -1,68 +1,119 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth  import authenticate,login
+from django.contrib.auth import authenticate,login
 from django.contrib.auth.models import User
 from django.contrib import messages
 
 
 def register_method(request):
-    errors={}
-    if request.method == "POST":
-        username = request.POST.get("username")
-        email = request.POST.get("email")
-        password = request.POST.get("password")
-        confirm = request.POST.get("confirm")
-        
+   
+    errors = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm = request.POST.get('confirm')
+
+        # print(username,email,password,confirm)
         if not username:
-            errors["username"]="username hal na mula"
+            errors['username'] = "Username is Required"
         elif User.objects.filter(username=username).exists():
-            errors["username"]="useralready exists"
+            errors['username'] = "Username already exists !! user another"
+
         if not email:
-            errors['email']='email chai kasla la halcha'
+            errors['email'] = "Email field is Required"
         elif User.objects.filter(email=email).exists():
-              errors["email"]="email already exists"
+            errors['email'] = "Email already exists ! user another email"
+        
         if not password:
-            errors['password']='daju halnu na password feri hacker la tanab dincha'
-        elif len(password)<=6:
-            errors['password']="password contains less then 6 character"
+            errors['password'] = "Password is required"
+        elif len(password) <=6:
+            errors['password']= "Password contains more than 6 characters"
+
         if not confirm:
-            errors["confirm"]="confirm hannu na daju"
+            errors['confirm'] = " Confirm Password is required"
+
         if password != confirm:
-            errors['confirm'] = "aakha ho ki guccha password aaautai vayana ni"
+            errors['confirm'] = "Confirm password not matched !!"
 
         if errors:
             return render(request,'auth/register_page.html',{'errors':errors,'data':request.POST})
         else:
-            user = User.objects.create(
+            user = User.objects.create_user(
                 username=username,
+                password=password,
                 email=email
             )
-            user.set_password(password)
             user.save()
-            messages.success(request,"user registration successfully")
-            return redirect('login')
+            messages.success(request,'User Registration Successfully !! ')
+            return redirect('login') 
     else:
         return render(request,'auth/register_page.html')
 
+
+# def login_method(request):
+#     errors = {}
+
+#     if request.method == 'POST':
+#         username= request.POST.get('username')
+#         password = request.POST.get('password')
+
+#         if not username:
+#             errors['username'] = "Username is Required"
+#         elif not User.objects.filter(username=username).exists():
+#             errors['username'] = "Username doesn't exists"
+
+
+#         if not password:
+#             errors['password'] = "Password is Required"
+
+#         # authenticate(request, username=username,password=password)
+#         # select username from User where email=email;
+#         if not errors:
+#             user = authenticate(request,username=username,password=password)
+#             if user is not None:
+#                 # session generate garxa 
+#                 login(request,user)
+#                 messages.success(request,'User logged in successfully')
+#                 return redirect('index')
+#             else:
+#                 errors['password'] = "Password is incorrect"
+#                 return render(request,'auth/login_page.html',{'errors':errors,'data':request.POST})
+#         else:
+#             return render(request,'auth/login_page.html',{'errors':errors,'data':request.POST})
+
+#     return render(request,'auth/login_page.html')
+
+
+
 def login_method(request):
+    
     errors = {}
 
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
-        
-        if not username:
-            errors['username'] = 'Username is required'
-        if not password:
-            errors['password'] = 'Password is required'
-        if errors:
-            return render(request, 'auth/login_page.html', {"errors": errors})
-        user = authenticate(request, username=username, password=password)
+    if request.method == 'POST':
+        email= request.POST.get('email')
+        password = request.POST.get('password')
 
-        if user is not None:
-            login(request, user)
-            messages.success(request, "User logged in successfully")
-            return redirect('index')
-    return render(request, 'auth/login_page.html')
-            
-            
-    
+        if not email:
+            errors['email'] = "Email is Required"
+        
+        if not User.objects.filter(email=email).exists():
+            errors['email'] = "Email doesn't exists"
+
+        if not password:
+            errors['password'] = "Password is Required"
+
+        if not errors:
+            check_user = User.objects.get(email=email)
+            user = authenticate(request,username=check_user.username,password=password)
+            if user is not None:
+                
+                login(request,user)
+                messages.success(request,'User logged in successfully')
+                return redirect('index')
+            else:
+                errors['password'] = "Password is incorrect"
+                return render(request,'auth/login_page.html',{'errors':errors,'data':request.POST})
+        else:
+            return render(request,'auth/login_page.html',{'errors':errors,'data':request.POST})
+
+    return render(request,'auth/login_page.html')
